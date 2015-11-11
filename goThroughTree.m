89410f59df5fdbb872a15ed7f4dc436e63c5b795
Matthieu <matthieuraymond@hotmail.com>
2015-11-11 00:20:11 +0000

    Try another decision between the 6 trees

--- goThroughTree.m
+++ goThroughTree.m
@@ -1,14 +1,20 @@
-function [ res ] = goThroughTree( tree, AU )
+function [ res, depth ] = goThroughTree( tree, AU )
 %GOTHROUGHTREE takes a row of AU values and process it through a tree
+%Returns the class and the depth
     if (isempty(tree.kids))
        res = tree.class;
+       depth = 0;
        return
     end
     
     if (AU(tree.op) == 0)
-        res = goThroughTree(tree.kids{1}, AU);
+        [a b] = goThroughTree(tree.kids{1}, AU);
+        res = a;
+        depth = b + 1;
     else
-        res = goThroughTree(tree.kids{2}, AU);
+        [a b] = goThroughTree(tree.kids{2}, AU);
+        res = a;
+        depth = b + 1;
     end
 end
 
--- predict.m
+++ predict.m
@@ -3,14 +3,31 @@ function [ res ] = predict( T, AU)
     %given in order
     % in T.
 
-    classification = zeros(6, 1);
+    classification = zeros(6, 2); %value, depth
 
     for i = 1:6
-       classification(i) = goThroughTree(T(i), AU); 
+       [classification(i,1), classification(i,2)] = goThroughTree(T(i), AU); 
     end
 
     % First attempt : taking the maximum
+    % Snd: the max farest from the root
 
-    [~, res] = max(classification);
+    m = classification(1,1);
+    d = classification(1,2);
+    res = 1;
+    
+    for i = 2:6
+       if classification(i,1) > m
+            m = classification(i,1);
+            d = classification(i,2);
+            res = i;
+       elseif classification(i,1) == m
+           if classification(i,2) > d
+                m = classification(i,1);
+                d = classification(i,2);
+                res = i;
+           end
+       end
+    end
 end
 
